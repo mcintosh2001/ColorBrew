799ec8fcbc690c63a14c0c7a60351575548c77f3
Andrew McIntosh <andrew.mcintosh@ed.ac.uk>
2021-12-27 20:39:16 +0000

    create function - avoid repitition

--- SomePlots.R
+++ SomePlots.R
@@ -15,111 +15,55 @@ My_Theme = theme(
   axis.title.y = element_text(size = 10),
   axis.text.y = element_text(size = 7)) 
 
-p1 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Hokusai1") +
-  scale_fill_manual(values=met.brewer("Hokusai1", 3)) +
-  My_Theme
-
-
-p2 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Hokusai2") +
-  scale_fill_manual(values=met.brewer("Hokusai2", 3)) +
-  My_Theme
+myplot <- function(scheme){
   
+  ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
+    geom_violin() +
+    labs(title = scheme) +
+    scale_fill_manual(values=met.brewer(scheme, 3)) +
+    My_Theme
+  
+}
 
-p3 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Hokusai3") +
-  scale_fill_manual(values=met.brewer("Hokusai3", 3)) +
-  My_Theme 
-
-
-p4 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Manet") +
-  scale_fill_manual(values=met.brewer("Manet", 3)) +
-  My_Theme
-
-p5 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Derain") +
-  scale_fill_manual(values=met.brewer("Derain", 3)) +
-  My_Theme
-
-p6 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Cassatt1") +
-  scale_fill_manual(values=met.brewer("Cassatt1", 3)) +
-  My_Theme
-
-p7 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Hiroshige") +
-  scale_fill_manual(values=met.brewer("Hiroshige", 3)) +
-  My_Theme
-
-p8 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "OKeeffe2") +
-  scale_fill_manual(values=met.brewer("OKeeffe2", 3)) +
-  My_Theme
-
-p9 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "OKeeffe1") +
-  scale_fill_manual(values=met.brewer("OKeeffe1", 3)) +
-  My_Theme
-
+p1 <- myplot("Hokusai1") 
+p2 <- myplot("Hokusai2")
+p3 <- myplot("Hokusai3")
+p4 <- myplot("Manet")
+p5 <- myplot("Derain") 
+p6 <- myplot("Cassatt1")
+p7 <- myplot("Hiroshige") 
+p8 <- myplot("OKeeffe2") 
+p9 <- myplot("OKeeffe1")
+  
 # cowplot command
 panel1 <- plot_grid(p1, p2, p3, p4, p5, p6, p7, p8, p9,
 ncol=3, labels = c('A', 'B','C','D','E','F'),
 label_size = 12)
 
 ggsave("panel1.tiff")
-
 rm(list = c(paste("p",c(1:6),sep="")))  
+rm(panel1)
 
 # RColorBrewer
 
-p1 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
+myplot1 <- function(scheme){
+  
+  ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
   geom_violin() +
-  labs(title = "Greens") +
-  scale_fill_brewer(palette = "Greens") +
+  labs(title = scheme) +
+  scale_fill_brewer(palette = scheme) +
   My_Theme
+}
 
-p2 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Blues") +
-  scale_fill_brewer(palette = "Blues") +
-  My_Theme
-
-p3 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Reds") +
-  scale_fill_brewer(palette = "Reds") +
-  My_Theme
-
-p4 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "Oranges") +
-  scale_fill_brewer(palette = "Oranges") +
-  My_Theme
-
-p5 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "YlOrBr") +
-  scale_fill_brewer(palette = "YlOrBr") +
-  My_Theme
-
-p6 <- ggplot(data=iris, aes(x=Species, y=Petal.Length, fill=Species)) +
-  geom_violin() +
-  labs(title = "YlOrRd") +
-  scale_fill_brewer(palette = "YlOrRd") +
-  My_Theme
+p1 <- myplot1("Greens") 
+p2 <- myplot1("Blues") 
+p3 <- myplot1("Reds") 
+p4 <- myplot1("Oranges") 
+p5 <- myplot1("YlOrBr") 
+p6 <- myplot1("YlOrRd") 
 
 # cowplot command
-panel1 <- plot_grid(p1, p2, p3, p4, p5, p6, ncol=3, labels = c('A', 'B','C','D','E','F'), label_size = 12)
+panel2 <- plot_grid(p1, p2, p3, p4, p5, p6, ncol=3, labels = c('A', 'B','C','D','E','F'), label_size = 12)
 ggsave("panel2.tiff")
-
+rm(list = c(paste("p",c(1:6),sep="")))  
+rm(panel2)
